57260fc27d878053c66dc05cbb72d6b0c5294ec6
atgm1113 <atgm1113@gmail.com>
2020-08-04 22:04:59 +0300

    Initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+
+setup(
+   name='kaggle_SIIM_ISIC',
+   version='1.0',
+   description='Personal project package',
+   author='atgm1113',
+   author_email='atgm1113@gmail.com',
+   packages=["src.data_process",
+             "src.main",
+             "src.models_build"])
